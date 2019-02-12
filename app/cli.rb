48a8aff07c4d71674727b1bb05a72819fd270343
ScottDenton <scottyd_14@hotmail.com>
2019-02-12 14:06:50 -0800

    built subject and date search - CLI

--- app/cli.rb
+++ app/cli.rb
@@ -14,6 +14,61 @@ class CLI
    end
 
    class Search
+     def self.search_loop
+       search_hash = {}
+       search_hash[:subject] = self.subject
+       while true
+         puts "Would you like to refine your search ? (y/n)"
+         refine = gets.chomp.downcase
+         if refine == 'n'
+           break
+         else
+           puts "What criteria would you like to add?"
+           puts "Location - Date - Keyword - Category"
+           criteria = gets.chomp.downcase
+
+           case criteria
+           when 'location'
+             self.location_search
+           when 'date'
+             dates = self.date_search
+             search_hash[:start] = dates[0]
+             search_hash[:end] = dates[1]
+           when 'keyword'
+             self.keyword_search
+           when 'category'
+             self.category_search
+           else
+             puts "Sorry we did not recogise your input"
+           end
+
+         end
+
+
+       end
+       search_hash
+     end
+
+     def self.location_search
+       puts 'location search'
+     end
+
+     def self.date_search
+       puts "What start date would you like ? yyyymmdd"
+       start_date = gets.chomp.downcase
+       puts "What end date would you like ? yyyymmdd"
+       end_date = gets.chomp.downcase
+       date = [start_date, end_date]
+
+     end
+
+     def self.keyword_search
+       puts 'keyword search'
+     end
+
+     def self.category_search
+       puts 'category search'
+     end
 
      def self.subject
          puts "What subject would you like to search for ?"
@@ -24,13 +79,12 @@ class CLI
        puts "Would you like to input a search date ?(y/n)"
        answer = gets.chomp.downcase
        if answer == 'y'
-         puts "What date would you like to search from"
+         puts "What date would you like to search from (yyyymmdd)"
          date = gets.chomp
        else
         date = nil
        end
      end
-
    end #end of search class
 
 end #end of CLI class
--- bin/run.rb
+++ bin/run.rb
@@ -5,33 +5,19 @@ require 'launchy'
 
 
 
-subject = CLI::Search.subject
-date = CLI::Search.date
-unless date.nil?
-  date_query = "&facet_field=day_of_week&facet=true&begin_date=#{date}"
-end 
+search = CLI::Search.search_loop
 
-  string = RestClient.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=#{subject}&api-key=#{NYT_KEY}#{date_query}")
+
+
+unless search[:start].nil?
+  date_query = "&facet_field=day_of_week&facet=true&begin_date=#{search[:start]}&end_date=#{search[:end]}"
+end
+# binding.pry
+
+  string = RestClient.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=#{search[:subject]}&api-key=#{NYT_KEY}#{date_query}")
+
   hash = JSON.parse(string)
 
   article = hash["response"]['docs'].first
    puts '.1 ' + article['snippet']
    puts '.2 ' + article['web_url']
-
-
-
-
-
-
-
-
-  # puts hash['title']
-  # link =  hash['img']
-  # puts hash['alt']
-  # puts 'type " open" to open a link to the image'
-  # response = gets.chomp
-  # if response = 'open'
-  #   Launchy.open(link)
-  # else
-  #   return
-  # end
