bb1efae87efce658afefa5799a7d2727fde4e4ab
github username <github email address>
2019-02-13 16:22:03 -0800

    added view.rb

---
+++ app/view.rb
@@ -0,0 +1,35 @@
+class View
+  def self.banner
+    system("clear")
+    banner = <<-BANNER
+
+          #     #                  #     #
+          ##    # ###### #    #     #   #   ####  #####  #    #
+          # #   # #      #    #      # #   #    # #    # #   #
+          #  #  # #####  #    #       #    #    # #    # ####
+          #   # # #      # ## #       #    #    # #####  #  #
+          #    ## #      ##  ##       #    #    # #   #  #   #
+          #     # ###### #    #       #     ####  #    # #    #
+
+                     #######
+                        #    # #    # ######  ####
+                        #    # ##  ## #      #
+                        #    # # ## # #####   ####
+                        #    # #    # #           #
+                        #    # #    # #      #    #
+                        #    # #    # ######  ####
+
+    BANNER
+    puts banner
+  end
+
+  def self.new_page
+    puts self.banner
+    puts ""
+  end
+
+  def self.header
+    puts "Welcome to the NYT CLI Search!"
+    puts "Type 'quit' at any time to exit."
+  end
+end
